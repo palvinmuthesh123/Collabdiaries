a63145baa9aaf86ad727aa2b829f05f3978f645b
Palvin Muthesh <spalvinmuthesh@gmail.com>
2024-12-24 18:21:15 +0530

    tttttttt

--- src/auth/firebase.service.ts
+++ src/auth/firebase.service.ts
@@ -1,18 +1,20 @@
 import * as admin from 'firebase-admin';
 import { Injectable } from '@nestjs/common';
 
+const privateKey = (
+  String(process.env.FIREBASE_PRIVATE_KEY_PART1) +
+  String(process.env.FIREBASE_PRIVATE_KEY_PART2) +
+  String(process.env.FIREBASE_PRIVATE_KEY_PART3)
+).replace(/\\n/g, '\n');
+
 @Injectable()
 export class FirebaseService {
   constructor() {
     admin.initializeApp({
       credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: (
-          process.env.FIREBASE_PRIVATE_KEY_PART1 +
-          process.env.FIREBASE_PRIVATE_KEY_PART2 +
-          process.env.FIREBASE_PRIVATE_KEY_PART3
-        ).replace(/\\n/g, '\n'),
+        projectId: String(process.env.FIREBASE_PROJECT_ID),
+        clientEmail: String(process.env.FIREBASE_CLIENT_EMAIL),
+        privateKey: privateKey,
       }),
     });
   }
